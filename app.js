9ab58d8a9da1865cd2b59e0848bd7b935398c822
Daviel Jimenez <Daviel050399@gmail.com>
2020-12-21 22:08:12 -0400

    Despliegue 1

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express();
+
+const port = 3002;
+app.use(express.static(__dirname + "/public"))
+
+//motor de plantillas
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+
+app.get("/", (req, res) => {
+
+    res.render("index", { titulo: "Index Dinamico" })
+})
+
+app.get("/servicios", (req, res) => {
+    res.render("servicios", { tituloServicios: "Pagina de Servicios🆘" })
+})
+
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        titulo: "404 EJS",
+        descripcion: "Descripcion del 404"
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor a su servicio en el puerto ${port}`)
+})
---
+++ frutas.js
@@ -0,0 +1,5 @@
+const frutas = ["banana", "banana", "pera", "banana"];
+
+const dinero = 1000;
+
+module.exports = { frutas, dinero };
