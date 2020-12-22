b8218b0da7696c04408ab705b16d0ce49caa96a0
Daviel Jimenez <Daviel050399@gmail.com>
2020-12-21 22:11:07 -0400

    Correcion de puerto dinamico

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
 
-const port = 3002;
+const port = process.env.PORT || 3002;
 app.use(express.static(__dirname + "/public"))
 
 //motor de plantillas
