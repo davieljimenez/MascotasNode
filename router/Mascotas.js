5fd67e80bb7f29762b34dfa41e42bc233352f091
Daviel Jimenez <Daviel050399@gmail.com>
2020-12-26 20:50:45 -0400

    CRUD COMPLETE

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -82,5 +82,24 @@ router.delete("/:id", async(req, res) => {
     }
 })
 
+router.put("/:id", async(req, res) => {
+    const id = req.params.id
+    const body = req.body
+    try {
+
+        const mascotaDB = await Mascota.findByIdAndUpdate(id, body, { useFindAndModify: false })
+        res.json({
+            estado: true,
+            mensaje: "Editado"
+        })
+    } catch (error) {
+        res.json({
+            estado: false,
+            mensaje: "Fallo al editar"
+        })
+
+    }
+})
+
 
 module.exports = router;
